1d682167941ec00dfc33937019a16220e284e15b
Krizhram <155589988+Krizhram@users.noreply.github.com>
2026-04-30 22:18:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-123"
     key            = "terraform-project/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
   }
 }
 
