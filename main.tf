51a7b84f802e8972244d9cf2b7f346939a10fa19
krizhram <krizhram@gmail.com>
2026-04-30 23:11:14 +0530

    updated region

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-new-st"
     key            = "terraform-project/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-1"
   }
 }
 
